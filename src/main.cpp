c99d051c3c59ee8aa83ff84b57bd11a0e61d6d90
drkwitht <derektan3510@gmail.com>
2024-02-23 10:18:47 -0800

    Added main REPL code to test.

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+#include <stdexcept>
+#include <iostream>
+#include "backend/parser.hpp"
+
+using MyMathExpr = eeval::ast::Expr;
+using MyAST = eeval::backend::ExprAST;
+
+void printInfo()
+{
+    std::cout << "ExprEval: evalutate arithmetic expressions.\n";
+    std::cout << "Enter \"end\" to stop the input to output loop.\n";
+}
+
+int main()
+{
+    eeval::backend::Parser parser {};
+    std::string input_string;
+
+    printInfo();
+
+    while (true)
+    {
+        std::cout << ">> ";
+        bool read_ok = std::getline(std::cin, input_string, '\n').fail();
+
+        if (!read_ok)
+        {
+            break;
+        }
+
+        if (input_string == "end")
+        {
+            break;
+        }
+
+        /// @note Catch all parse exceptions gracefully to avoid unexpected program quit.
+        try
+        {
+            MyAST parse_tree = parser.parseSource(input_string);
+
+            std::cout << evaluateIt(parse_tree) << '\n';
+        }
+        catch (std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+
+        input_string.clear();
+    }
+
+    return 0;
+}
